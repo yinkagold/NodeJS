2c1cde199eba0357c9ee27d4bda77c59225651b1
Yinka Gold <yinkagolden@gmail.com>
2018-06-05 12:17:44 +0300

    Assingning a port and using env variables

--- express-demo/index.js
+++ express-demo/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    res.send('Hello world!!!');
 });
 app.get('/api/courses', (req, res) =>{
     res.send([1,2,3]);
 });
-app.listen(3000, () => console.log('Listening on port 3000'));
+
+//PORT 
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ... ${port}`));
