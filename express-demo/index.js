a53f28d51f5374b0901e6d103ef8fcd130b0260f
Yinka Gold <yinkagolden@gmail.com>
2018-06-08 16:02:08 +0300

    Third party middleware and process env

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,8 +1,27 @@
+const morgan = require('morgan');
+const helmet = require('helmet');
 const Joi = require('joi');
+const logger = require('./logger');
 const express = require('express');
 const app = express();
 
+
 app.use(express.json()); // this enables the name object 
+app.use(express.urlencoded({ extended: true }));// middleware
+app.use(express.static('public'))// middleware 
+app.use(helmet());
+//shows what the code is running on 
+if(app.get('env') === 'development'){
+    app.use(morgan('tiny'));
+    console.log('Morgan enabled...')
+}
+
+app.use(function(req, res, next) {
+    console.log('Logging ...');
+    next();// this passes the control to another middleware function 
+}); // called to install middleware function 
+
+app.use(logger);
 
 const courses = [
     { id: 1, name: 'course1'},
@@ -17,12 +36,11 @@ app.get('/api/courses', (req, res) =>{
     const course = courses.find(c => c.id === parseInt(req.params.id));   //Look up the course
     if(!course) return res.status(404).send('The course with the given ID was not found.');    
     res.send(course);
-
 });
 
 app.post('/api/courses', (req, res) =>{
     const { error } = validateCourse(req.body); // result.error // object destruction 
-    if(error) return res.status(400).send(result.error.details[0].message);       // 400 Bad Request
+    if(error) return res.status(400).send(error.details[0].message);       // 400 Bad Request
     const course = {
         id: courses.length + 1 , 
         name: req.body.name
@@ -36,7 +54,7 @@ app.put('/api/courses/:id', (req, res) =>{
     if(!course) return res.status(404).send('The course with the given ID was not found.');     //If invalid, return 404 
 
     const { error } = validateCourse(req.body); // result.error
-    if(error) return res.status(400).send(result.error.details[0].message); // 400 Bad Request
+    if(error) return res.status(400).send(error.details[0].message); // 400 Bad Request
      
     course.name = req.body.name; //update course 
     res.send(course); //Return the updated course 
