bc52fc0e4ae482a1248e41286e63a4636f2fb4ee
Yinka Gold <yinkagolden@gmail.com>
2018-06-09 18:52:56 +0300

    Using Async and Wait

--- async-demo/index.js
+++ async-demo/index.js
@@ -14,11 +14,22 @@ function getCommits(repos){
 function displayCommits(commits){
     console.log(commits);
 }
-getUser(1)
-    .then(user => getRepositories(user.gitHubUsername))
-    .then(repos => getCommits(repos[0]))
-    .then(commits => console.log('Commits', commits))
-    .catch(err =>  console.log('Error', err.message))
+
+//Promise based approach
+// getUser(1)
+//     .then(user => getRepositories(user.gitHubUsername))
+//     .then(repos => getCommits(repos[0]))
+//     .then(commits => console.log('Commits', commits))
+//     .catch(err =>  console.log('Error', err.message));
+
+    //Async and Await Approach 
+async function displayCommits(){
+    const user = await getUser(1);
+    const repos = await getRepositories(user.gitHubUsername);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+}
+displayCommits();
 function getUser(id){
     return new Promise((resolve, reject) =>{
         //kick off asyn work
