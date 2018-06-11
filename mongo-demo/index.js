17a8a9a1f203b930f244c008cea4a4d2619e7c96
Yinka Gold <yinkagolden@gmail.com>
2018-06-11 13:36:45 +0300

    Update db using Query first by Id

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -13,8 +13,7 @@ mongoose.connect('mongodb://localhost/playground')
     });
 const Course = mongoose.model('Course', courseSchema); //class 
 async function createCourse(){
- // schema is compiled into a model
-  
+ // schema is compiled into a model 
     const course = new Course({
         name: 'Angular Course',
         author: 'Yinka',
@@ -26,32 +25,31 @@ async function createCourse(){
     const result = await course.save();
     console.log(result);
 }
-// retrieving functions 
-//eq (equal)
-//ne (not equal)
-//gt (greater than)
-//gte (greater than or equal to)
-// lt (less than)
-//lte (less than or equal)
-// in 
-// nin (not in)
-async function getCourses(){
-    const courses = await Course
-   // .find({ author: 'Yinka', isPublished: true }) // filter result
-   //.find({ price: { $gt: 10 }}) //filter with price >10 using $comparison operator
-   // .find({ price: { $gte: 10, $lte: 20 }}) // between 10 and 20
-    //.find({ price: { $in: [10, 15, 20] } }) 
-   
-    .find({author: /^Yinka/})  // find using regualr expressions // starts with Yinka 
-    .find({author: /Gold$/i }) // ends with Gold , i makes it case insentive
-    .find({ author: /.*Yinka.*/}) // Contains Yinka
-    .or([{author: 'Yinka'}, {isPublished: true}]) // using logical operator or
-    .and([{author: 'Yinka'}, {isPublished: true}])
-   .limit(10)
-    .sort({ name: 1}) // 1 indicates acscending order, -1 descending order
-    .select({ name: 1, tags: 'node' }) // selects properties to be returned 
-    console.log(courses);
-}
-getCourses();
 
 //createCourse();
+
+// async function getCourses(){
+//     const courses = await Course
+//     .find({author: 'Yinka', isPublished: true})
+//     .sort({ name: 1}) // 1 indicates acscending order, -1 descending order
+//     .select({ name: 1, tags: 'node' }) // selects properties to be returned 
+//     .count(); // counts number of document  
+//     console.log(courses);
+// }
+// getCourses();
+// Query first approach 
+async function updateCourse(id){
+    const course = await Course.findById(id);
+
+    if (!course) return;
+
+   
+    course.isPublished = true;
+    course.author = 'Another Author';
+
+    const result = await course.save();
+    console.log(result);
+   
+}
+
+updateCourse('5b1cecb69a2c8d5f601317f8');
