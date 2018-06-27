791af43906619d360de37e653ac77aca3bd167f6
Yinka Gold <yinkagolden@gmail.com>
2018-06-27 12:32:29 +0300

    using built in validators

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -5,55 +5,47 @@ mongoose.connect('mongodb://localhost/playground')
     .catch(err => console.log('Could not connect to MongoDB...', err));
 
     const courseSchema = new mongoose.Schema({
-        name: String,
+        name: {
+             type: String,
+             required: true,
+             minlength: 5,
+             maxlength: 255
+        }, // using validation 
+        category: {
+            type: String,
+            required: true,
+            enum: ['web', 'mobile', 'network'] 
+        },
         author: String,
         tags: [String], 
         date: { type: Date, default: Date.now },
-        isPublished: Boolean
+        isPublished: Boolean,
+        Price: { 
+            type: Number,
+            required: function() { return this.isPublished; },
+            min: 20,
+            max: 200
+        }
     });
 const Course = mongoose.model('Course', courseSchema); //class 
 async function createCourse(){
  // schema is compiled into a model 
     const course = new Course({
         name: 'Angular Course',
+        category: 'web',
         author: 'Yinka',
         tags: ['angular', 'frontend'],
-        isPublished: true
+        isPublished: true,
+        price: 15
     });
-    
+    try{
     //saving to Database   
-    const result = await course.save();
-    console.log(result);
+      const result = await course.save();
+      console.log(result);
+    }catch(ex){
+        console.log(ex.message);
+    }
+   
 }
 
-//createCourse();
-
-// async function getCourses(){
-//     const courses = await Course
-//     .find({author: 'Yinka', isPublished: true})
-//     .sort({ name: 1}) // 1 indicates acscending order, -1 descending order
-//     .select({ name: 1, tags: 'node' }) // selects properties to be returned 
-//     .count(); // counts number of document  
-//     console.log(courses);
-// }
-// getCourses();
-// Query first approach 
-async function updateCourse(id){
-    //const result = await Course.update( { _id: id }, {
-    const result = await Course.findByIdAndUpdate(  id , {
-        $set: {
-            author: 'Bob',
-            isPublished: false
-        }
-    }, {new: true }); // returns the updated document 
-    console.log(result);
-}
-
-//updateCourse('5b1ced4592fdde2408c77fe9');
-
-async function removeCourse(id){
-// const result = await Course.deleteOne({ _id:id });
- const course = await Course.findByIdAndRemove(id);
- console.log(course)
-}
-removeCourse('5b1ced4592fdde2408c77fe9');
+createCourse();
